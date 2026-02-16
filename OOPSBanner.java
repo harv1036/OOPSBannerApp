f9623716ffc83eae4fdd5d840d7176c652ab5bee
harv1036 <harv1032006@gmail.com>
2026-02-16 19:44:04 +0530

    Coded to Render O0PS as Banner using Print Statements

---
+++ OOPSBanner.java
@@ -0,0 +1,13 @@
+public class OOPSBanner
+{
+	public static void main(String args[])
+	{
+		System.out.println("  *****  "+" "+"*******  "+" "+"*******  "+" "+"  *******");
+		System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**     **");
+		System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
+		String.out.println("**     **"+" "+"*******  "+" "+"*******  "+" "+"  *****  ");
+		System.out.println("**     **"+" "+"**       "+" "+"**       "+" "+"       **");
+		System.out.println("**     **"+" "+"**       "+" "+"**       "+" "+"**     **");
+		System.out.println("  *****  "+" "+"**       "+" "+"**       "+" "+"*******  ");
+	}
+}
