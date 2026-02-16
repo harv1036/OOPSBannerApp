574f25ef20319780a41f6e9b4f143a7a7a65af2b
Harsh Jain <harv1032006@gmail.com>
2026-02-16 23:16:41 +0530

    Refactor main method for OOPSBanner class

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,13 +1,16 @@
 public class OOPSBanner
 {
-	public static void main(String args[])
-	{
-		System.out.println("  *****  "+" "+"*******  "+" "+"*******  "+" "+"  *******");
-		System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**     **");
-		System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
-		String.out.println("**     **"+" "+"*******  "+" "+"*******  "+" "+"  *****  ");
-		System.out.println("**     **"+" "+"**       "+" "+"**       "+" "+"       **");
-		System.out.println("**     **"+" "+"**       "+" "+"**       "+" "+"**     **");
-		System.out.println("  *****  "+" "+"**       "+" "+"**       "+" "+"*******  ");
-	}
+    public static void main(String[] args) {
+
+        System.out.println("    ***    " + "    ***    " + " ******    " + "    *****  ");
+        System.out.println("  **   **  " + "  **   **  " + " **    **  " + "  **       ");
+        System.out.println(" **     ** " + " **     ** " + " **     ** " + " **        ");
+        System.out.println(" **     ** " + " **     ** " + " **    **  " + "  **       ");
+        System.out.println(" **     ** " + " **     ** " + " ******    " + "    ***    ");
+        System.out.println(" **     ** " + " **     ** " + " **        " + "       **  ");
+        System.out.println(" **     ** " + " **     ** " + " **        " + "        ** ");
+        System.out.println("  **   **  " + "  **   **  " + " **        " + "       **  ");
+        System.out.println("    ***    " + "    ***    " + " **        " + "  *****    ");
+
+    }
 }
